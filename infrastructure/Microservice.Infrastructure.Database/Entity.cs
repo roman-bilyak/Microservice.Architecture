ee6f5f39a83652be4fb7f46aa90b9d83298392c4
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2021-11-26 14:58:58 +0200

    added draft of database infrastructure

---
+++ infrastructure/Microservice.Infrastructure.Database/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace Microservice.Infrastructure.Database;
+
+public class Entity : Entity<Guid>
+{
+}
+
+public class Entity<T> where T : notnull
+{
+    public T Id { get; set; }
+}
---
+++ infrastructure/Microservice.Infrastructure.Database/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Microservice.Infrastructure.Database;
+
+public interface IRepository<T> : IReadRepository<T, Guid>
+    where T : class, IAggregateRoot
+{
+}
+
+public interface IRepository<T, TId> : IReadRepository<T, TId>
+    where T : class, IAggregateRoot
+    where TId : notnull
+{
+    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+}
