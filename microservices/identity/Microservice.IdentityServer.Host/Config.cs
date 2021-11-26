29cdb092303d004b20bdbae0dc59eb0defde7a57
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2021-11-26 10:10:31 +0200

    added draft identity server

---
+++ microservices/identity/Microservice.IdentityServer.Host/Config.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.Models;
+
+namespace Microservice.IdentityServer.Host;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+
+        };
+}
