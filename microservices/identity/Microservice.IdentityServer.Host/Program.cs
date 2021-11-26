1446a372dd40c999f97ddcea1ba5127632cdf21a
Roman Bilyak <bilyak.roman@gmail.com>
2021-11-26 10:10:31 +0200

    added draft identity server

---
+++ microservices/identity/Microservice.IdentityServer.Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microservice.IdentityServer.Host;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer(options =>
+    {
+        // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients)
+    .AddDeveloperSigningCredential(); //not recommended for production - you need to store your key material somewhere secure
+
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseIdentityServer();
+
+app.Run();
