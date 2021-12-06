e864675582679ef12c1f5185bc4bde3adccf8770
Roman Bilyak <bilyak.roman@gmail.com>
2021-12-07 01:50:03 +0200

    implemented product service infractructure module

---
+++ microservices/product/Microservice.ProductService.Infrastructure/Database/Configurations/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microservice.ProductService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Microservice.ProductService.Infrastructure.Database;
+
+internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(30);
+    }
+}
---
+++ microservices/product/Microservice.ProductService.Infrastructure/Database/ProductServiceDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microservice.Infrastructure.Database.EntityFrameworkCore;
+using Microservice.ProductService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.ProductService.Infrastructure.Database;
+
+internal class ProductServiceDbContext : BaseDbContext<ProductServiceDbContext>
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ProductServiceDbContext(DbContextOptions<ProductServiceDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductServiceDbContext).Assembly);
+    }
+}
