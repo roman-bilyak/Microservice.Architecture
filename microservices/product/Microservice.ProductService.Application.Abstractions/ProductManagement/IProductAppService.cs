780e647b62ceee62c9f69389bd7c8c634ccb88c2
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2021-12-06 14:55:33 +0200

    implemented draft of product service application abstractions

---
+++ microservices/product/Microservice.ProductService.Application.Abstractions/ProductManagement/IProductAppService.cs
@@ -0,0 +1,15 @@
+﻿namespace Microservice.ProductService.Application;
+
+public interface IProductAppService
+{
+    public Task<ProductDto> GetAsync(Guid id);
+
+    public Task<List<ProductDto>> GetAllAsync();
+
+    public Task<ProductDto> CreateAsync(CreateProductDto product);
+
+    public Task<ProductDto> UpdateAsync(UpdateProductDto product);
+
+    public Task DeleteAsync(Guid id);
+
+}
