62eddf6ecabe76988b8f74e59d0f6e91b2fbb324
Roman Bilyak <bilyak.roman@gmail.com>
2021-12-06 14:38:41 +0200

    moved initialization of web application into the appropriate module

---
+++ microservices/product/Microservice.ProductService.Host/ProductServiceHostModule.cs
@@ -0,0 +1,40 @@
+﻿using Microservice.AspNetCore;
+using Microservice.Core.Modularity;
+using Microsoft.OpenApi.Models;
+
+namespace Microservice.ProductService.Host;
+
+public sealed class ProductServiceHostModule : Module
+{
+    public override void Configure(IServiceCollection services)
+    {
+        base.Configure(services);
+
+        services.AddControllers();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Product Service API", Version = "v1" });
+        });
+    }
+
+    public override void Initialize(IServiceProvider serviceProvider)
+    {
+        base.Initialize(serviceProvider);
+
+        IApplicationBuilder app = serviceProvider.GetApplicationBuilder();
+
+        app.UseDeveloperExceptionPage();
+
+        app.UseRouting();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
+
+        app.UseEndpoints(x => x.MapDefaultControllerRoute());
+    }
+}
