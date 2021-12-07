e59ca999d0372be3b8737469702a0eb17cfa2435
Roman Bilyak <bilyak.roman@gmail.com>
2021-12-07 02:08:31 +0200

    implemented API methods of product service

---
+++ microservices/product/Microservice.ProductService.Application/ProductServiceApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using Microservice.Core.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.ProductService.Application;
+
+public sealed class ProductServiceApplicationModule : Module
+{
+    public override void Configure(IServiceCollection services)
+    {
+        base.Configure(services);
+
+        services.AddTransient<IProductAppService, ProductAppService>();
+    }
+}
