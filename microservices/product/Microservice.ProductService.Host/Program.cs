d11af26418d057e36b02dd18e15dc69853e62bde
Roman Bilyak <bilyak.roman@gmail.com>
2021-12-07 02:04:01 +0200

    added missed modules to application

--- microservices/product/Microservice.ProductService.Host/Program.cs
+++ microservices/product/Microservice.ProductService.Host/Program.cs
@@ -1,11 +1,17 @@
 using Microservice.AspNetCore;
 using Microservice.Core;
+using Microservice.ProductService.Application;
+using Microservice.ProductService.Domain;
 using Microservice.ProductService.Host;
+using Microservice.ProductService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication()
-    .AddModule<ProductServiceHostModule>()
     .AddModule<AspNetCoreModule>()
+    .AddModule<ProductServiceDomainModule>()
+    .AddModule<ProductServiceInfrastructureModule>()
+    .AddModule<ProductServiceApplicationModule>()
+    .AddModule<ProductServiceHostModule>()
     .Configure();
 
 var app = builder.Build();
