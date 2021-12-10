22b4ab203c58dce9c264078198f33f0e9652e0b1
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2021-12-10 19:57:34 +0200

    added 'Review' module API interfaces

---
+++ microservices/review/Microservice.ReviewService.Application.Abstractions/Reviews/GetUserReviewsDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Microservice.ReviewService.Application.Reviews;
+
+public class GetUserReviewsDto : List<ReviewDto>
+{
+}
---
+++ microservices/review/Microservice.ReviewService.Application.Abstractions/Reviews/IMovieApplicationService.cs
@@ -0,0 +1,6 @@
+﻿namespace Microservice.ReviewService.Application.Reviews;
+
+public interface IMovieApplicationService
+{
+    public Task<GetMovieReviewsDto> GetMovieReviewsAsync(Guid id, CancellationToken cancellationToken);
+}
