25ae004d1ee9530b10f78dcd44bb32a75e56ffd6
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2021-12-17 09:53:34 +0200

    refactored logic of dynamic controller registration

---
+++ infrastructure/Microservice.AspNetCore/Conventions/DynamicControllerSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Microservice.AspNetCore.Conventions;
+
+internal class DynamicControllerSettings
+{
+    public Assembly Assembly { get; private set; }
+
+    public Func<Type, bool> TypePredicate { get; private set; }
+
+    public DynamicControllerSettings(Assembly assembly, Func<Type, bool> typePredicate)
+    {
+        ArgumentNullException.ThrowIfNull(assembly);
+        ArgumentNullException.ThrowIfNull(typePredicate);
+
+        Assembly = assembly;
+        TypePredicate = typePredicate;
+    }
+}
