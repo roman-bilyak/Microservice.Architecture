6b14c5538617901e37f04718209ffb7f370fef47
Roman Bilyak <bilyak.roman@gmail.com>
2021-12-24 20:35:01 +0200

    added deployment kustomization file

---
+++ deploy-to-k8s.ps1
@@ -0,0 +1,10 @@
+﻿minikube start
+
+& minikube -p minikube docker-env | Invoke-Expression
+docker-compose -f docker-compose.yml -f docker-compose.override.yml build
+
+kubectl apply -k ./k8s
+
+Start-Sleep -s 10
+
+kubectl port-forward deployment.apps/gateway 51500:80
