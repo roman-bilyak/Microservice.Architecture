389f12d20caff85a2fc102b68b95bc6da4a8d879
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2022-01-12 14:59:55 +0200

    added NBomber configs to generate and view performance metrics

---
+++ tests/Microservice.Performance.Tests/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using NBomber.Configuration;
+
+namespace Microservice.Performance.Tests
+{
+    internal class CommandLineOptions
+    {
+        [Option('u', "url", Required = true, HelpText = "Urls to be processed.")]
+        public IEnumerable<string> Urls { get; set; }
+
+        [Option('t', "timeout", Default = 5, Required = false, HelpText = "Step timeout (seconds).")]
+        public int Timeout { get; set; }
+
+        [Option('c', "config", Required = false, HelpText = "Configuration file.")]
+        public string Config { get; set; }
+
+    }
+}
