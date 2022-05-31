a7d4f3bf3436790b2c0a1e39d8fea0b1aa9d793f
Roman Bilyak <bilyak.roman@gmail.com>
2022-05-31 22:44:15 +0300

    added proxies for API methods

---
+++ applications/Microservice.AngularUI/src/app/service-proxies/jwt-bearer-interceptor.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtBearerInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request);
+  }
+}
