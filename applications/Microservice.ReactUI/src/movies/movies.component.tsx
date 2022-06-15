b5d23ad086bc70b38f1f121ac4a853f6cab3df9d
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2022-06-16 00:58:01 +0300

    ReactUI: added component to show list of movies

---
+++ applications/Microservice.ReactUI/src/movies/movies.component.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import { MovieAPIService, MovieDto } from "../api-services/api-services";
+
+export const MoviesComponent = () => {
+  const [items, setItems] = useState<MovieDto[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+
+  const columns = [
+    {
+      name: 'Title',
+      selector: (row: MovieDto) => row.title || ''
+    }];
+
+  const fetchData = async (page: number, size: number | undefined = perPage) => {
+    setLoading(true);
+
+    let movieAPIService = new MovieAPIService('http://localhost:7100');
+    var response = await movieAPIService.getMovies(page - 1, size);
+
+    setItems(response.items || []);
+    setTotalCount(response.totalCount);
+    setLoading(false);
+  };
+
+  const handlePageChange = async (page: number) => {
+    await fetchData(page);
+    setCurrentPage(page);
+  };
+
+  const handlePerRowsChange = async (newPerPage: number, page: number) => {
+    await fetchData(page, newPerPage);
+    setPerPage(newPerPage);
+  };
+
+  return (
+    <DataTable
+      columns={columns}
+      data={items}
+
+      progressPending={loading}
+
+      pagination
+      paginationServer
+
+      paginationTotalRows={totalCount}
+      paginationDefaultPage={currentPage}
+      paginationPerPage={perPage}
+
+      onChangeRowsPerPage={handlePerRowsChange}
+      onChangePage={handlePageChange}
+    />
+  );
+}
