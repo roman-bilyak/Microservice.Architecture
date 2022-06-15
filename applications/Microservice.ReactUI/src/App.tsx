fe766a4bf169bfded174e1ac834ba841b1bac368
Roman Bilyak <bilyak.roman@gmail.com>
2022-06-16 01:22:52 +0300

    ReactUI: added router

--- applications/Microservice.ReactUI/src/App.tsx
+++ applications/Microservice.ReactUI/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MoviesComponent } from './movies/movies.component';
 
 function App() {
   return (
-    <div className="App">
-      <MoviesComponent />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<MoviesComponent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
