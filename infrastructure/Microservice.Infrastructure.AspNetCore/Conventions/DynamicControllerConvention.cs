7b65de0a6087c2b84ea7817442d694497b974c9a
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2022-09-03 02:58:29 +0300

    supported Authorize attribute for dynamic controllers

--- infrastructure/Microservice.Infrastructure.AspNetCore/Conventions/DynamicControllerConvention.cs
+++ infrastructure/Microservice.Infrastructure.AspNetCore/Conventions/DynamicControllerConvention.cs
@@ -3,110 +3,182 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.Extensions.Options;
+using System.Linq;
 using System.Reflection;
 
 namespace Microservice.Infrastructure.AspNetCore.Conventions;
 
 public class DynamicControllerConvention : IApplicationModelConvention
 {
-    private readonly DynamicControllerOptions _options;
-
-    public DynamicControllerConvention(IOptions<DynamicControllerOptions> options)
-    {
-        _options = options.Value;
-    }
-
     public void Apply(ApplicationModel application)
     {
         foreach (ControllerModel controller in application.Controllers)
         {
-            Type controllerType = controller.ControllerType.AsType();
-
-            controller.ControllerName = GetControllerName(controllerType);
-            controller.ApiExplorer.GroupName = controller.ControllerName;
-            controller.ApiExplorer.IsVisible = true;
-
-            if (!controller.Selectors.Any(selector => selector.AttributeRouteModel != null))
-            {
-                controller.Selectors.Add(new SelectorModel
-                {
-                    AttributeRouteModel = new AttributeRouteModel(new RouteAttribute("api")),
-                });
-            }
+            NormalizeName(controller);
+            SetGroupName(controller);
+            SetVisibility(controller);
+            SetRoute(controller);
 
             foreach (ActionModel action in controller.Actions)
             {
-                action.ActionName = action.ActionName.RemoveSuffix("Async");
-                action.ApiExplorer.IsVisible = IsActionVisible(action.ActionName);
-
-                action.Selectors.Where(x => x.AttributeRouteModel == null).ToList()
-                    .ForEach(s => action.Selectors.Remove(s));
-
-                SelectorModel selector = new SelectorModel
-                {
-                    AttributeRouteModel = new AttributeRouteModel(new RouteAttribute($"{controller.ControllerName}/{action.ActionName}")),
-                };
-
-                string methodVerb = GetMethodVerbByActionName(action.ActionName);
-                selector.ActionConstraints.Add(new HttpMethodActionConstraint(new[] { methodVerb }));
-                action.Selectors.Add(selector);
-
-                foreach (ParameterModel parameter in action.Parameters)
-                {
-                    if (methodVerb != "GET" && methodVerb != "DELETE"
-                        && !IsPrimitiveType(parameter.ParameterInfo.ParameterType)
-                        && !typeof(CancellationToken).IsAssignableFrom(parameter.ParameterInfo.ParameterType))
-                    {
-                        parameter.BindingInfo = BindingInfo.GetBindingInfo(new[] { new FromBodyAttribute() });
-                    }
-                }
+                ConfigureAction(controller, action);
             }
         }
     }
 
     #region helper methods
 
-    private string GetControllerName(Type controllerType)
+    private static void NormalizeName(ControllerModel controller)
     {
-        string controllerName = controllerType.Name;
+        string controllerName = controller.ControllerType.AsType().Name;
         if (controllerName.StartsWith("I") && controllerName.Length > 1 && char.IsUpper(controllerName[1]))
         {
             controllerName = controllerName.Substring(1, controllerName.Length - 1);
         }
-        return controllerName.RemoveSuffix("Proxy").RemoveSuffix("ApplicationService");
+        controller.ControllerName = controllerName
+            .RemoveSuffix("Controller")
+            .RemoveSuffix("ApplicationService")
+            .RemoveSuffix("Proxy");
     }
 
-    private bool IsActionVisible(string actionName)
+    private static void SetGroupName(ControllerModel controller)
     {
-        return actionName != "DynProxyGetTarget"
+        if (!string.IsNullOrEmpty(controller.ApiExplorer.GroupName))
+        {
+            return;
+        }
+        controller.ApiExplorer.GroupName = controller.ControllerName;
+    }
+
+    private static void SetVisibility(ControllerModel controller)
+    {
+        if (controller.ApiExplorer.IsVisible.HasValue)
+        {
+            return;
+        }
+        controller.ApiExplorer.IsVisible = true;
+    }
+
+    private static void SetRoute(ControllerModel controller)
+    {
+        if (!controller.Selectors.Any())
+        {
+            controller.Selectors.Add(new SelectorModel());
+        }
+
+        foreach (var selector in controller.Selectors)
+        {
+            if (selector.AttributeRouteModel == null)
+            {
+                selector.AttributeRouteModel = new AttributeRouteModel(new RouteAttribute("api"));
+            }
+        }
+    }
+
+    private static void ConfigureAction(ControllerModel controller, ActionModel action)
+    {
+        NormalizeName(action);
+        SetVisibility(action);
+        SetRoute(action);
+        ConfigureParameters(action);
+    }
+
+    private static void NormalizeName(ActionModel action)
+    {
+        string actionName = action.ActionName.RemoveSuffix("Async");
+        action.ActionName = actionName;
+    }
+
+    private static void SetVisibility(ActionModel action)
+    {
+        if (action.ApiExplorer.IsVisible.HasValue)
+        {
+            return;
+        }
+
+        string actionName = action.ActionName;
+        action.ApiExplorer.IsVisible = actionName != "DynProxyGetTarget"
             && actionName != "DynProxySetTarget"
-            && actionName != "GetInterceptors";
+            && actionName != "GetInterceptors"; ;
     }
 
-    public static string GetMethodVerbByActionName(string actionName)
+    private static void SetRoute(ActionModel action)
     {
-        if (actionName.StartsWith("Get", StringComparison.OrdinalIgnoreCase))
+        if (!action.Selectors.Any())
+        {
+            action.Selectors.Add(new SelectorModel());
+        }
+
+        foreach (var selector in action.Selectors)
+        {
+            string? httpMethod = selector.ActionConstraints
+                .OfType<HttpMethodActionConstraint>()
+                .FirstOrDefault()?
+                .HttpMethods?
+                .FirstOrDefault();
+
+            if (httpMethod == null)
+            {
+                httpMethod = GetHttpMethodByActionName(action);
+            }
+
+            if (selector.AttributeRouteModel == null)
+            {
+                selector.AttributeRouteModel = new AttributeRouteModel(new RouteAttribute("[controller]/[action]"));
+            }
+
+            if (!selector.ActionConstraints.OfType<HttpMethodActionConstraint>().Any())
+            {
+                selector.ActionConstraints.Add(new HttpMethodActionConstraint(new[] { httpMethod }));
+            }
+        }
+    }
+
+    private static string GetHttpMethodByActionName(ActionModel action)
+    {
+        string actionName = action.ActionName;
+
+        if (actionName.StartsWith("Get", StringComparison.InvariantCultureIgnoreCase))
         {
             return "GET";
         }
 
-        if (actionName.StartsWith("Create", StringComparison.OrdinalIgnoreCase))
+        if (actionName.StartsWith("Create", StringComparison.InvariantCultureIgnoreCase))
         {
             return "POST";
         }
 
-        if (actionName.StartsWith("Update", StringComparison.OrdinalIgnoreCase))
+        if (actionName.StartsWith("Update", StringComparison.InvariantCultureIgnoreCase))
         {
             return "PUT";
         }
 
-        if (actionName.StartsWith("Delete", StringComparison.OrdinalIgnoreCase))
+        if (actionName.StartsWith("Delete", StringComparison.InvariantCultureIgnoreCase))
         {
             return "DELETE";
         }
 
         return "POST";
+    }
+
+    private static void ConfigureParameters(ActionModel action)
+    {
+        var httpMethods = action.Selectors
+            .SelectMany(x => x.ActionConstraints.OfType<HttpMethodActionConstraint>())?
+            .SelectMany(x => x.HttpMethods) ?? new string[] { };
+
+        bool useFormBodyBinding = !httpMethods.Contains("GET")
+            && !httpMethods.Contains("DELETE");
+
+        foreach (ParameterModel parameter in action.Parameters)
+        {
+            if (useFormBodyBinding
+                && !IsPrimitiveType(parameter.ParameterInfo.ParameterType)
+                && !typeof(CancellationToken).IsAssignableFrom(parameter.ParameterInfo.ParameterType))
+            {
+                parameter.BindingInfo = BindingInfo.GetBindingInfo(new[] { new FromBodyAttribute() });
+            }
+        }
     }
 
     private static bool IsPrimitiveType(Type type)
