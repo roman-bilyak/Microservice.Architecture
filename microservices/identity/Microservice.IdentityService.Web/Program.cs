ab0100443f6d635b258ef401207167de5e90d9b2
Roman Bilyak <bilyak.roman@gmail.com>
2022-09-12 15:09:58 +0300

    added IdentityServer4 quick start UI

--- microservices/identity/Microservice.IdentityService.Web/Program.cs
+++ microservices/identity/Microservice.IdentityService.Web/Program.cs
@@ -2,7 +2,15 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServer()
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+    })
     .AddTestUsers(Config.TestUsers.ToList())
     .AddInMemoryIdentityResources(Config.IdentityResources)
     .AddInMemoryApiResources(Config.ApiResources)
@@ -11,7 +19,15 @@ builder.Services.AddIdentityServer()
     .AddDeveloperSigningCredential(); //not recommended for production - you need to store your key material somewhere secure
 
 var app = builder.Build();
+
 app.UseDeveloperExceptionPage();
+
+app.UseStaticFiles();
+app.UseRouting();
+
 app.UseIdentityServer();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 app.Run();
