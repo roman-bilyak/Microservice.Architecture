3d7f46425e117832d7e2d68d8c25d4593d2d30c3
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2022-09-14 18:32:22 +0300

    added missed file

---
+++ core/Microservice.Swagger/Filters/IgnorePathsFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Microservice.Swagger;
+
+internal class IgnorePathsFilter : IDocumentFilter
+{
+    private readonly SwaggerOptions _options;
+
+    public IgnorePathsFilter(IOptions<SwaggerOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        if (_options.IgnorePaths is null)
+        {
+            return;
+        }
+
+        foreach (string ignorePath in _options.IgnorePaths)
+        {
+            swaggerDoc.Paths.Remove(ignorePath);
+        }
+    }
+}
