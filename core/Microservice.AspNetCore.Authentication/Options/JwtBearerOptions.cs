8771d1087a31ba482445ff72b45597c40785b2e4
Roman Bilyak <bilyak.roman@gmail.com>
2022-09-30 00:32:55 +0300

    fixed running application using docker-compose

---
+++ core/Microservice.AspNetCore.Authentication/Options/JwtBearerOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservice.AspNetCore.Authentication;
+
+public class JwtBearerOptions
+{
+    public string Authority { get; set; }
+
+    public string ValidIssuer { get; set; }
+
+    public string Audience { get; set; }
+
+    public bool? RequireHttpsMetadata { get; set; }
+}
