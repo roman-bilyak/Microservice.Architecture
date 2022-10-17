a94fd6fbd510c301b46fd9d5bdc4e287d87eef60
Roman Bilyak <bilyak.roman@gmail.com>
2022-10-17 11:28:58 +0300

    tested sending messages using rabbitmq

--- microservices/test/Microservice.TestService.Application/Tests/TestApplicationService.cs
+++ microservices/test/Microservice.TestService.Application/Tests/TestApplicationService.cs
@@ -1,11 +1,30 @@
-﻿using Microservice.Application.Services;
+﻿using MassTransit;
+using Microservice.Application.Services;
 
 namespace Microservice.TestService.Tests;
 
 internal class TestApplicationService : ApplicationService, ITestApplicationService
 {
+    private readonly IBus _bus;
+
+    public TestApplicationService(IBus bus)
+    {
+        _bus = bus;
+    }
+
     public Task GetAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
+    }
+
+    public async Task SendMessageAsync(int id, string message, CancellationToken cancellationToken)
+    {
+        TestMessage testMessage = new TestMessage
+        {
+            Id = id,
+            Message = message
+        };
+
+        await _bus.Publish(testMessage, cancellationToken);
     }
 }
--- microservices/test/Microservice.TestService.Web/TestServiceWebModule.cs
+++ microservices/test/Microservice.TestService.Web/TestServiceWebModule.cs
@@ -1,5 +1,7 @@
-﻿using Microservice.Api;
+﻿using MassTransit;
+using Microservice.Api;
 using Microservice.Core.Modularity;
+using Microservice.TestService.Tests;
 
 namespace Microservice.TestService;
 
@@ -8,4 +10,23 @@ namespace Microservice.TestService;
 [DependsOn(typeof(ApiModule))]
 public sealed class TestServiceWebModule : StartupModule
 {
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        base.ConfigureServices(services);
+
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<TestMessageConsumer>();
+
+            x.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host("localhost", "/", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                cfg.ConfigureEndpoints(ctx);
+            });
+        });
+    }
 }
