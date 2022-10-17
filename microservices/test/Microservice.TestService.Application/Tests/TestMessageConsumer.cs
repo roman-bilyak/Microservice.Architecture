ecc93c29c5a732c643428732fdec00a96d5b6daa
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2022-10-17 11:28:58 +0300

    tested sending messages using rabbitmq

---
+++ microservices/test/Microservice.TestService.Application/Tests/TestMessageConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Microservice.TestService.Tests;
+
+public class TestMessageConsumer : IConsumer<TestMessage>
+{
+    readonly ILogger<TestMessageConsumer> _logger;
+
+    public TestMessageConsumer(ILogger<TestMessageConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<TestMessage> context)
+    {
+        _logger.LogInformation("Test Message: {Id} - {Message}", context.Message?.Id, context.Message?.Message);
+        return Task.CompletedTask;
+    }
+}
