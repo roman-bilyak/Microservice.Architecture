b8f424788543e18daa518d681799b3f4cfaad5f1
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-01-26 17:12:53 +0200

    added UI to Duende Identity Server

--- applications/Microservice.AuthServer/HostingExtensions.cs
+++ applications/Microservice.AuthServer/HostingExtensions.cs
@@ -7,7 +7,7 @@ namespace Microservice.AuthServer
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
             // uncomment if you want to add a UI
-            //builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
             builder.Services.AddIdentityServer(options =>
                 {
@@ -31,14 +31,14 @@ namespace Microservice.AuthServer
             }
 
             // uncomment if you want to add a UI
-            //app.UseStaticFiles();
-            //app.UseRouting();
+            app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseIdentityServer();
 
             // uncomment if you want to add a UI
-            //app.UseAuthorization();
-            //app.MapRazorPages().RequireAuthorization();
+            app.UseAuthorization();
+            app.MapRazorPages().RequireAuthorization();
 
             return app;
         }
---
+++ applications/Microservice.AuthServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace Microservice.AuthServer.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
