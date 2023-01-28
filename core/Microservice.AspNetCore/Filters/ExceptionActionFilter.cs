fbce371a5f8f430857d954520eead448a5498242
Roman Bilyak <rb@gmail.com>
2023-01-28 15:08:42 +0200

    added draft action filters to validate input fields and handle exceptions

---
+++ core/Microservice.AspNetCore/Filters/ExceptionActionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace Microservice.AspNetCore;
+
+internal class ExceptionActionFilter : IAsyncExceptionFilter
+{
+    public Task OnExceptionAsync(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        //TODO: return detailed errors in response
+        context.Result = new ObjectResult(context.Exception.Message);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ core/Microservice.AspNetCore/Filters/ValidationActionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Microservice.AspNetCore;
+
+internal class ValidationActionFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            //TODO: throw specific exception with error details
+            throw new Exception("ModelState is not valid!");
+        }
+
+        await next();
+    }
+}
