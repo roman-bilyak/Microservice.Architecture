119f6d7cf6eef5c1ff28892812c13f127726ea12
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-01-29 00:11:52 +0200

    fixed warnings

--- core/Microservice.Core/Application.cs
+++ core/Microservice.Core/Application.cs
@@ -11,6 +11,7 @@ internal class Application<TStartupModule> : IApplication
 {
     private readonly Action<ApplicationConfigurationOptions>? _configurationOptionsAction;
     private List<IStartupModule>? _modules;
+    private IServiceProvider? _serviceProvider;
 
     public Application(IServiceCollection services, IConfiguration configuration,
         Action<ApplicationConfigurationOptions>? configurationOptionsAction)
@@ -36,8 +37,6 @@ internal class Application<TStartupModule> : IApplication
 
     public IConfiguration Configuration { get; private set; }
 
-    public IServiceProvider? ServiceProvider { get; private set; }
-
     public virtual void ConfigureServices()
     {
         Services.AddSingleton<IApplication>(this);
@@ -51,32 +50,39 @@ internal class Application<TStartupModule> : IApplication
         _configurationOptionsAction?.Invoke(configurationOptions);
     }
 
+    public virtual IServiceProvider GetServiceProvider()
+    {
+        EnsureInitialized();
+
+        return _serviceProvider;
+    }
+
     public virtual void SetServiceProvider(IServiceProvider serviceProvider)
     {
         ArgumentNullException.ThrowIfNull(serviceProvider);
 
-        if (ServiceProvider != null && ServiceProvider != serviceProvider)
+        if (_serviceProvider != null && _serviceProvider != serviceProvider)
         {
             throw new Exception("Service provider was already set before to another service provider instance.");
         }
 
-        ServiceProvider = serviceProvider;
+        _serviceProvider = serviceProvider;
     }
 
     public virtual void Configure()
     {
         EnsureInitialized();
 
-        Modules.ForEach(x => x.PreConfigure(ServiceProvider));
-        Modules.ForEach(x => x.Configure(ServiceProvider));
-        Modules.ForEach(x => x.PostConfigure(ServiceProvider));
+        Modules.ForEach(x => x.PreConfigure(_serviceProvider));
+        Modules.ForEach(x => x.Configure(_serviceProvider));
+        Modules.ForEach(x => x.PostConfigure(_serviceProvider));
     }
 
     public virtual void Shutdown()
     {
         EnsureInitialized();
 
-        Modules.ForEach(x => x.Shutdown(ServiceProvider));
+        Modules.ForEach(x => x.Shutdown(_serviceProvider));
     }
 
     public virtual void Dispose()
@@ -85,12 +91,12 @@ internal class Application<TStartupModule> : IApplication
 
     #region helper methods
 
-    [MemberNotNull(nameof(ServiceProvider))]
+    [MemberNotNull(nameof(_serviceProvider))]
     private void EnsureInitialized()
     {
-        if (ServiceProvider is null)
+        if (_serviceProvider is null)
         {
-            throw new InvalidOperationException($"{nameof(ServiceProvider)} not yet been initialized.");
+            throw new InvalidOperationException($"{nameof(_serviceProvider)} not yet been initialized.");
         }
     }
 
