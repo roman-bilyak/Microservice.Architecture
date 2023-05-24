51f6fadd4150aa14ef1635dd4dbc2e2199b488d3
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-05-24 13:41:54 +0300

    added scripts to start and stop minikube

--- k8s/build.ps1
+++ k8s/build.ps1
@@ -3,21 +3,6 @@
     [string] $tag = ''
 )
 
-echo '📌 Start minikube'
-
-$enableRegistryAddon = $registry -eq 'localhost:5000'
-$registryAddon = $(If ($enableRegistryAddon) { 'registry' } else { 'fake' })
-
-minikube start --insecure-registry '10.0.0.0/24' --addons dashboard --addons ingress --addons $registryAddon
-
-If ($enableRegistryAddon)
-{
-    Remove-Job -Name RunJob* -Force
-	
-    Start-Job -Name RunJobPortForward -ScriptBlock { kubectl port-forward --namespace kube-system service/registry 5000:80 } | Out-Null
-    Start-Job -Name RunJobRedirectPort -ScriptBlock { docker run --rm --network=host alpine ash -c 'apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000' } | Out-Null
-}
-
 echo '📌 Build images'
 
 $Env:DOCKER_REGISTRY = $registry
