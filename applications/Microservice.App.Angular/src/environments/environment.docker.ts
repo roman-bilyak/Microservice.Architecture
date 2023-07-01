269717bfca3196bd7c90885978efc554efe7d7ad
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-07-01 13:18:05 +0300

    added docker environments to Angular application

--- applications/Microservice.App.Angular/src/environments/environment.docker.ts
+++ applications/Microservice.App.Angular/src/environments/environment.docker.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
 
-  baseUrl: "https://localhost",
+  baseUrl: "https://localhost:9001",
 
-  authority: "https://localhost/auth",
+  authority: "https://localhost:9100",
   clientId: "api_client",
   clientScope: "api-identity-service api-movie-service api-payment-service api-review-service api-test-service",
 
-  apiUrl: 'https://localhost/api'
+  apiUrl: 'https://localhost:9200'
 };
